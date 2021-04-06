634d16fce383e22ae04014c1a72166c38f9c7a91
Josiah Sprankle <jspranklemusic@gmail.com>
2021-04-06 09:51:09 -0500

    commit

--- functions/test.js
+++ functions/test.js
@@ -1,10 +1,6 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({
-        bananas:2,
-        apples:7,
-        fruits:this.apples + this.bananas
-      })
+      body: JSON.stringify(event)
     })
   }
