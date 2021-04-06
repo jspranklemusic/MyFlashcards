713c6b8919d233ab72c43a8226f3bae5794e0d0c
Josiah Sprankle <jspranklemusic@gmail.com>
2021-04-06 10:15:22 -0500

    commit

--- functions/test.js
+++ functions/test.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=>{
+    console.log('connected!')
+  })
+  .catch(err=>{
+    console.log(err)
+  });
+
+
+  const User = mongoose.model('User', new mongoose.Schema({
+    data:Object
+  }));
 
 
 exports.handler = (event, context, callback) => {
