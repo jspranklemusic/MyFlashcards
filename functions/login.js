4b4c43fb9f99655f8f89e366489aaa518c59e5d8
Josiah Sprankle <jspranklemusic@gmail.com>
2021-04-06 10:32:05 -0500

    message

--- functions/login.js
+++ functions/login.js
@@ -14,8 +14,9 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 
 
 exports.handler = (event, context, callback) => {
+    let bananas = 7
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(event)
+      body: JSON.stringify(bananas)
     })
   }
