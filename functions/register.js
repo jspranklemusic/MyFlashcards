c36b530dd1bf72459adf004292368ac127efdee8
Josiah Sprankle <jspranklemusic@gmail.com>
2021-04-06 11:08:17 -0500

    message

--- functions/register.js
+++ functions/register.js
@@ -14,11 +14,12 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 
 
 exports.handler = async (event, context) => {
+    
 
     const user = new User({
         data:{
-            username:event?.body?.username,
-            password:event?.body?.password,
+            username:event.body.username,
+            password:event.body.password,
             flashcards:[],
             notes:[]
         }
