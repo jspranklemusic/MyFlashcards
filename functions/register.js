a1064a7e247b522a69ec6d3bb2e2849bb4281482
Josiah Sprankle <jspranklemusic@gmail.com>
2021-04-06 11:27:50 -0500

    message

--- functions/register.js
+++ functions/register.js
@@ -4,8 +4,9 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     console.log('connected!')
   })
   .catch(err=>{
-    console.log(err)
+    console.log("Error", err)
   });
+
 
   console.log(process.env.MONGO_URI)
 
